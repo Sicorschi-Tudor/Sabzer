63a78a608a57d5da6e1070232a33cad4bd114ed7
Tudor Sicorschi <aaw1713tudor@gmail.com>
2022-08-13 14:08:51 +0300

    Dropdown

--- js.js
+++ js.js
@@ -65,3 +65,39 @@ const message = () => {
 
 
 message();
+
+
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const list = dropdown.querySelector('.list');
+    const options = dropdown.querySelectorAll('.list li');
+    const selected = dropdown.querySelector('.selected');
+
+
+select.addEventListener('click', () => {
+
+    select.classList.toggle('select-clicked');
+    caret.classList.toggle('caret-rotate');
+    list.classList.toggle('list-open');
+});
+options.forEach(option => {
+    option.addEventListener('click',() => {
+        selected.innerText = option.innerText;
+        select.classList.remove('select-clicked');
+        caret.classList.remove('caret-rotate');
+        list.classList.remove('list-open');
+        options.forEach(option => {
+option.classList.remove('active');
+        });
+        option.classList.add('active');
+    });
+});
+
+
+
+
+});
