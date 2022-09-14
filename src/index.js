88ce0dd786a2b48165621d121ddb7120c94aea33
Tudor Sicorschi <aaw1713tudor@gmail.com>
2022-09-14 20:54:03 +0300

    ..

--- src/index.js
+++ src/index.js
@@ -141,6 +141,19 @@ const message = (e) => {
 
 
   
+
+        document.getElementById('send').addEventListener('click', 
+function(){
+
+document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+document.getElementById('close').addEventListener('click', 
+function(){
+
+document.querySelector('.bg-modal').style.display = 'none';
+});
+
         
     
    
